d6652fe861933b542b8cc4a39d86d6f5b215b6c6
0Zix0 <zixerino@gmail.com>
2017-03-26 00:12:48 +1100

    Episode 5 - Vertex Array

--- Client/src/com/mineclone/gfx/Model.java
+++ Client/src/com/mineclone/gfx/Model.java
@@ -3,34 +3,58 @@ package com.mineclone.gfx;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.util.ArrayList;
 
 public class Model {
 
-	private int vertexID;
+	private ArrayList<Integer> buffers = new ArrayList<Integer>();
+	private int vboCount = 0;
+	private int vao;
 	
 	public Model(float[] vertexPositions) {
-		vertexID = glGenBuffers();
-		bind();
-		glBufferData(GL_ARRAY_BUFFER, createFloatBuffer(vertexPositions), GL_STATIC_DRAW);
-		glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0);
-		glEnableVertexAttribArray(0);
-		unbind();
-	}
-	
-	public void bind() {
-		glBindBuffer(GL_ARRAY_BUFFER, vertexID);
-	}
-	
-	public void unbind() {
+		vao = glGenVertexArrays();
+		
+		glBindVertexArray(vao);
+		
+		addVBO(2, vertexPositions);
+		
+		glBindVertexArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 	}
 	
+	public void addVBO(int dim, float[] data) {
+		int vbo = glGenBuffers();
+		
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, createFloatBuffer(data), GL_STATIC_DRAW);
+		glVertexAttribPointer(vboCount, dim, GL_FLOAT, false, 0, 0);
+		glEnableVertexAttribArray(vboCount);
+		vboCount++;
+		buffers.add(vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+	
+	public void bind() {
+		glBindVertexArray(vao);
+	}
+	
+	public void unbind() {
+		glBindVertexArray(0);
+	}
+	
 	public void delete() {
-		glDeleteBuffers(vertexID);
+		glDeleteVertexArrays(vao);
+		
+		//for(int i : buffers) glDeleteBuffers(i);
+		buffers.forEach((Integer i) -> {
+			glDeleteBuffers(i);
+		});
 	}
 	
 	private static FloatBuffer createFloatBuffer(float[] array) {
